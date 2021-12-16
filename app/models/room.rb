8bfbae2ccbb171c21ad05b803e6886e5e78f9380
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2021-12-16 15:02:32 +0100

    Depentence destroy

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   belongs_to :hotel
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   validates :price_per_night, presence: true, numericality: { greater_than: 0 }
