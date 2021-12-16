92c29cf659d9c732a6b159091cc8597b0f1fbd09
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2021-12-17 00:35:44 +0100

    Front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @rooms = Room.last(4)
+    @rooms = Room.last(3)
   end
 end
